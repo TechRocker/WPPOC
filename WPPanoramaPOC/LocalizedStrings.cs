49defd9b9e5778e6545527487ef429de47de09a3
TechRocker <arupghosh_it@hotmail.com>
2014-12-15 22:57:31 +0530

    creat project

---
+++ WPPanoramaPOC/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using WPPanoramaPOC.Resources;
+
+namespace WPPanoramaPOC
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
